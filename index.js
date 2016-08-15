a2783128516e7ef8451b2e81c05d64c55de44658
Wayne Bontrager <wbontrager@live.com>
2016-08-14 19:32:56 -0500

    initial fitbit oauth2 authroization setup

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
+var FitbitApiClient = require("fitbit-node");
+var client = new FitbitApiClient(process.env.FITBIT_CLIENT_ID, process.env.FITBIT_CLIENT_SECRET);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -25,6 +28,35 @@ app.get('/times', function(request, response) {
       result += i + ' ';
   response.send(result);
 });
+
+// Fitbit API Authorization
+var authorizeCallbackUrl = "https://hidden-reef-88758.herokuapp.com/fitbit/callback";
+var accessCallbackUrl = "https://hidden-reef-88758.herokuapp.com/fitbit/callback_2";
+app.get("/fitbit/authorize", function (req, res) {
+  console.log('Fitbit authorization started');
+  res.redirect(client.getAuthorizeUrl('activity profile sleep social', authorizeCallbackUrl));
+});
+
+app.get("/fitbit/callback", function (req, res) {
+  console.log('Fitbit callback hit');
+  client.getAccessToken(req.query.code, accessCallbackUrl).then(function (result) {
+    console.log('  Fitbit callback getAccessToken success');
+    client.get("/profile.json", result.access_token).then(function (results) {
+      console.log('    Fitbit callback get profile success');
+      console.log('    result: ' + results);
+      res.send(results[0]);
+    });
+  }).catch(function (error) {
+    console.log('  Fitbit callback getAccessToken error');
+    res.send(error);
+  });
+});
+
+app.get("/fitbit/callback_2", function (req, res) {
+  console.log('Fitbit callback 2 hit');
+  response.send("callback 2" + cool());
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
